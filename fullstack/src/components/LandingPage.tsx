39c2871348a7bf0995d22d36b35c7cae825d78e4
Leonor Aidos <leonoraidos98@gmail.com>
2023-05-03 19:10:50 +0100

    Merge resolve.... again

--- fullstack/src/components/LandingPage.tsx
+++ fullstack/src/components/LandingPage.tsx
@@ -11,8 +11,8 @@ function LandingPage() {
   return (
   <div className='flex flex-col justify-center items-center h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
     <Image src={logoTransparent} width={1500} height={1500} alt="comping-machine-logo"/>
-    <h1 className='mt-12 mb-12'>Jam right in your browser</h1>
-    <button className="bg-zinc-200  border-white opacity-30 w-52 h-16 rounded-xl font-semibold text-xl" onClick={() => signIn( undefined, { callbackUrl: '/playground' })}>Get started</button>
+    <h1 className='mt-12 mb-12 font-semibold text-6xl text-white'>Jam right in your browser</h1>
+    <button className="bg-zinc-200 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border border-gray-100 border-white w-52 h-16 rounded-xl font-semibold text-xl hover:bg-opacity-80" onClick={() => signIn( undefined, { callbackUrl: '/playground' })}>Get started</button>
   </div>
   )
 }
